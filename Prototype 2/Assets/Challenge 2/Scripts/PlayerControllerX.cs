16e7d20cc773e64069c88ef77f35910b4ce9cbc0
Lương Linh Khôi <luongkhoi3456@gmail.com>
2026-02-06 08:30:10 +0700

    feat: Complete basic Challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float cooldownTime = 2.0f;
+
+    bool isCooldownActive = false;
+
+    void ResetCooldownTime()
+    {
+       isCooldownActive = false;
+    }
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !isCooldownActive)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            isCooldownActive = true;
+            Invoke("ResetCooldownTime", cooldownTime);
+        }
+    }
+}
