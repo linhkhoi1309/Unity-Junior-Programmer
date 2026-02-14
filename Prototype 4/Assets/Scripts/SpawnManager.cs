bac510c33f2727a9767c6718a80beabe3e5208c9
Lương Linh Khôi <luongkhoi3456@gmail.com>
2026-02-15 03:24:52 +0700

    feat: Complete Lesson 4.4

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,43 @@
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using Vector3 = UnityEngine.Vector3;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+
+    public int waveNumber = 1;
+    public int enemyCount;
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
     }
     private Vector3 GenerateSpawnPosition()
     {
         float randomX = Random.Range(-spawnRange, spawnRange);
         float randomZ = Random.Range(-spawnRange, spawnRange);
         return new Vector3(randomX, 0, randomZ);
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    void Update()
+    {
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+            SpawnEnemyWave(waveNumber);
+        }
     }
 }
